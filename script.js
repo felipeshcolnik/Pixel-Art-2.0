0f1dbe0f6ce7e95cfb28c42b2ea93078f7fe65ce
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-05-18 13:45:05 -0300

    create sizeBoard fct

--- script.js
+++ script.js
@@ -4,17 +4,8 @@ window.onload = function() {
 
 numberOfColors = 5;
 
-// const createColorOptions = (numberOfColors) => {
-//   for (let i = 0; i < numberOfColors; i =+ 1) {
-//     const divMae = document.getElementById('color-palette');
-//     const div = document.createElement('div');
-//     div.className = 'color-palette';
-//     divMae.appendChild(div);
-//   };
-// };
-
 function createColorOptions(numberOfColors) {
-  const divMae = document.getElementById('color-palette');  
+  const divMae = document.getElementById('color-palette-list');  
   for (let i = 0; i < numberOfColors; i += 1) {
     const div = document.createElement('div');
     div.className = 'color-palette';
